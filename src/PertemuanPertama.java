9b5ce7c09d6131ffb7c451c28d77ca2e854bb72c
oriorasajambu <oriorasajambu@gmail.com>
2020-12-31 12:22:58 +0700

    First Commit

--- src/PertemuanPertama.java
+++ src/PertemuanPertama.java
@@ -1,2 +1,73 @@
+/*
+ * Copyright (c) 2020.  Silahkan di copy paste guys
+ * Kalau ada yg mau di tanya silahkan di chat via Group WA / request di Github
+ * Sekalian follow di instagram @riowiraawan
+ */
+
+/*
+    Pertemuan Pertama (Comment, Menampilkan Text, Deklarasi Variable, Operasi Aritmatik, I/O)
+
+    1. Comment -> catatan
+        Multi Line Comment  -> //
+        1 Line Comment      -> //
+
+    2. Menampilkan Text
+        System.out.print("Hello World");
+        System.out.println("Hello World");
+
+    3. Deklarasi variable
+
+        tipe_data nama_variable;
+        tipe_data nama_variable2 = data;
+
+        tipe_data nama_variable;
+        nama_variable = data;
+
+        tipe_data nama_variable1 = data, nama_variable2, nama_variable3;
+        nama_variable2 = data;
+
+        tipe data
+        int     -> bilangan bulat   cth 0, 1, 2, 3, -3, -123
+        double  -> bilangan berkoma cth 0.64, 10.3, 13.324
+        float   -> bilangan berkoma cth 123f
+        String  -> kalimat          cth "Hello World"
+        char    -> Karakter         cth 'A', 'B'
+        boolean -> true / false
+
+        Cth Deklarasi Variable
+
+        int umur;
+        int umurSaya = 27;
+
+        int bilanganPertama;
+        bilanganPertama = 10;
+
+        int x , y , z = 1;
+        x = 2;
+
+    4. Operasi Aritmatik
+        +   ->  tambah
+        -   ->  kurang
+        *   ->  kali
+        /   ->  bagi
+        %   ->  modulus (Sisa Pembagian)
+
+    5. I/O
+
+        import java.util.Scanner;
+        Scanner input = new Scanner(System.in);
+
+        int x = input.nextInt();
+ */
+
+import java.util.Scanner;
+
 public class PertemuanPertama {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+    }
+
 }
